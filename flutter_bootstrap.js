ac718cc6fa9ff16f9ddcade3a2fce78701724231
DutkinTE <dutkinte@gmail.com>
2024-04-14 15:54:55 +0300

    update domain

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"0734f47f2a3df93a92f33c60db717522597e89
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4088529176"
+    serviceWorkerVersion: "3479041928"
   }
 });
